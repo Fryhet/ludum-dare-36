8e10b9020c28249c9de1bffb8512c89cc62ba31f
Milan Vaško <m.vasko2@gmail.com>
2016-08-28 10:50:40 +0200

    Level selection

---
+++ game/level_select/level_select.gd
@@ -0,0 +1,19 @@
+extends Control
+
+export(PackedScene) var menu_entry
+
+func _ready():
+	var container = get_node("container")
+	for i in range(0, global.TEMPLATE_COUNT):
+		var entry = menu_entry.instance()
+		container.add_child(entry)
+		var button = entry.get_node("button")
+		button.set_text(str("Level ", i + 1))
+		button.connect("pressed", self, "_on_level_selected", [i])
+
+func _on_level_selected(index):
+	global.template_to_load = index
+	get_tree().change_scene("res://game/building.tscn")
+
+func _on_main_menu_pressed():
+	get_tree().change_scene("res://game/main_menu.tscn")
--- game/main_menu.gd
+++ game/main_menu.gd
@@ -3,3 +3,7 @@ extends Container
 func _on_play_pressed():
 	global.template_to_load = 0
 	get_tree().change_scene("res://game/building.tscn")
+
+
+func _on_level_select_pressed():
+	get_tree().change_scene("res://game/level_select/level_select.tscn")
