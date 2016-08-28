03ea29b3d50672a2763fcec8528164890d6ac626
Milan Vaško <m.vasko2@gmail.com>
2016-08-28 21:57:56 +0200

    Made the sand texture bigger

--- game/ingame/ground.gd
+++ game/ingame/ground.gd
@@ -1,13 +1,13 @@
 tool
-extends StaticBody2D
+extends Node2D
 
 export(Texture) var texture
 var shape
 
 func _ready():
-	shape = get_node("shape")
+	shape = get_node("../shape")
 
 func _draw():
-	var extents = shape.get_shape().get_extents() * 2.0 * shape.get_scale()
+	var extents = shape.get_shape().get_extents() * 2.0 * shape.get_scale() / get_scale()
 	var pos = shape.get_pos() - (extents / 2.0)
 	draw_texture_rect(texture, Rect2(pos, extents), true)
