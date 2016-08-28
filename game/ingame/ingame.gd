1c6e51eef8205cd8bb0918009872cdc9e5cb4c19
Milan Vaško <m.vasko2@gmail.com>
2016-08-28 18:09:11 +0200

    New background. Also fixed finishing last level.

--- game/ingame/ingame.gd
+++ game/ingame/ingame.gd
@@ -1,6 +1,8 @@
 extends Node
 
 func _ready():
+	var background_scene = load(str("res://game/backgrounds/background_", randi() % global.BACKGROUND_COUNT, ".tscn"))
+	get_node("background").add_child(background_scene.instance())
 	var template_scene = load(str("res://game/templates/template_", global.template_to_load, ".tscn"))
 	var template = template_scene.instance()
 	get_node("template").add_child(template)
@@ -18,7 +20,7 @@ func _on_finished_pressed():
 
 	if global.game_mode == global.GAME_MODE_NORMAL:
 		if global.template_to_load >= global.TEMPLATE_COUNT - 1:
-			get_tree().change_scene("res://game/end/end.tscn")
+			get_tree().change_scene("res://game/after_level/after_level_end.tscn")
 		else:
 			get_tree().change_scene("res://game/after_level/after_level_normal.tscn")
 	elif global.game_mode == global.GAME_MODE_SELECT:
