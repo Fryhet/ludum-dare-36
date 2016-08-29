fd361af6b62d96038a6a5e7fd11525c1adca07d4
Milan Vaško <m.vasko2@gmail.com>
2016-08-29 15:39:31 +0200

    Added a panel to the after level scene

--- game/after_level/stats.gd
+++ game/after_level/stats.gd
@@ -6,15 +6,15 @@ var max_blocks
 
 func set_shape_precision(precision):
 	self.precision = precision
-	get_node("container/shape_precision").set_text(str("Shape precision: ", to_percent_string(precision)))
+	get_node("panel/container/shape_precision").set_text(str("Shape precision: ", to_percent_string(precision)))
 
 func set_blocks_used(blocks, max_blocks):
 	self.blocks = blocks
 	self.max_blocks = max_blocks
-	get_node("container/blocks_used").set_text(str("Blocks used: ", blocks, "/", max_blocks))
+	get_node("panel/container/blocks_used").set_text(str("Blocks used: ", blocks, "/", max_blocks))
 
 func generate_verdict():
-	get_node("verdict").set_text(str('"', precision_verdict(precision), block_verdict(blocks, max_blocks), '"'))
+	get_node("panel/verdict").set_text(str('"', precision_verdict(precision), block_verdict(blocks, max_blocks), '"'))
 
 func precision_verdict(score):
 	if global.template_to_load == 0:
@@ -32,7 +32,7 @@ func precision_verdict(score):
 		return "Not too bad."
 	if score > 0.20:
 		return "Come on, you can do better!"
-	return "What is this? Do you want to move these rocks by hand?"
+	return "What is this?! Do you want to move these rocks by hand?"
 
 func block_verdict(blocks, max_blocks):
 	if blocks > max_blocks:
