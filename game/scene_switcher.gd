a202fbb1cab20c740830b982b3c8af86fe4e74ed
Milan Vaško <m.vasko2@gmail.com>
2016-08-29 20:37:23 +0200

    Fixed a nasty restart bug and added a final level
    Also, the game is officially named Pharablocks!

--- game/scene_switcher.gd
+++ game/scene_switcher.gd
@@ -6,5 +6,7 @@ func _ready():
 	container = get_node("/root/main/container")
 
 func switch_scene(scene):
-	container.get_child(0).queue_free()
-	container.add_child(scene.instance())
+	var current = container.get_child(0)
+	current.set_name("this_is_a_name_which_should_not_collide_with_anything_else")
+	current.queue_free()
+	container.add_child(scene.instance(), true)
