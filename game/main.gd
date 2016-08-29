210f8f55d671017face8f5080a36f8f3cb4ce8c2
Milan Vaško <m.vasko2@gmail.com>
2016-08-29 19:13:49 +0200

    Added the most important button - the sound switch

--- game/main.gd
+++ game/main.gd
@@ -1,4 +1,7 @@
-extends Container
+extends Control
+
+func _ready():
+	get_node("sounds").set_pressed(AudioServer.get_fx_global_volume_scale() > 0.5)
 
 func _on_play_pressed():
 	sample_player.play("button")
@@ -11,6 +14,12 @@ func _on_level_select_pressed():
 func _on_help_pressed():
 	sample_player.play("button")
 	scene_switcher.switch_scene(load("res://game/help/help.tscn"))
+
+func _on_sounds_toggled(pressed):
+	if pressed:
+		AudioServer.set_fx_global_volume_scale(1.0)
+	else:
+		AudioServer.set_fx_global_volume_scale(0.0)
 
 func _on_exit_pressed():
 	get_tree().quit()
