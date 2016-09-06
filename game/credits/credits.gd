db3ea29bcadc132a68ba04c8606b7b8c8bfe72e3
Milan Vaško <m.vasko2@gmail.com>
2016-09-06 13:17:18 +0200

    The whole game should be localizable
    The slovak translation went away for now, though.

--- game/credits/credits.gd
+++ game/credits/credits.gd
@@ -5,7 +5,18 @@ func _input(event):
 		_on_main_menu_pressed()
 
 func _ready():
+	translate()
 	set_process_input(true)
+
+func translate():
+	var pavol = get_node("panel/pavol_molcsan")
+	pavol.set_bbcode(pavol.get_bbcode().replace("GRAPHICS", tr("GRAPHICS")))
+	var milan = get_node("panel/milan_vasko")
+	milan.set_bbcode(milan.get_bbcode().replace("PROGRAMMING", tr("PROGRAMMING")))
+	var sounds = get_node("panel/sounds")
+	sounds.set_bbcode(sounds.get_bbcode().replace("CREDITS_SOUNDS", tr("CREDITS_SOUNDS")))
+	var ludum_dare = get_node("panel/created_in")
+	ludum_dare.set_bbcode(ludum_dare.get_bbcode().replace("CREDITS_LUDUM_DARE", tr("CREDITS_LUDUM_DARE")))
 
 func _on_main_menu_pressed():
 	sample_player.play("button")
--- game/help/help.gd
+++ game/help/help.gd
@@ -1,7 +1,22 @@
 extends Control
 
 func _ready():
+	translate()
 	set_process_input(true)
+
+func translate():
+	var tab_container = get_node("tab_container")
+	tab_container.set_tab_title(0, tr("THE_STORY"))
+	tab_container.set_tab_title(1, tr("BLOCKS"))
+
+	var the_story = get_node("tab_container/the_story/text")
+	the_story.set_bbcode(the_story.get_bbcode()\
+		.replace("STORY_0", tr("STORY_0"))\
+		.replace("STORY_1", tr("STORY_1"))\
+		.replace("STORY_2", tr("STORY_2"))\
+		.replace("STORY_3", tr("STORY_3"))\
+	)
+	pass
 
 func _input(event):
 	if event.type == InputEvent.KEY && event.is_action("ui_cancel") && !event.is_pressed():
